d999775281410b6b332f6f2bdc682c2e201e96d0
Guilherme Oliveira Delmiglio <guidelmiglio@hotmail.com>
2016-04-04 20:51:07 -0300

    Configuração API

--- ChamadaApp.Web/ChamadaApp.API/App_Start/WebApiConfig.cs
+++ ChamadaApp.Web/ChamadaApp.API/App_Start/WebApiConfig.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
+﻿using Newtonsoft.Json.Serialization;
 using System.Web.Http;
-using Microsoft.Owin.Security.OAuth;
-using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
 
 namespace ChamadaApp.API
 {
@@ -13,9 +9,9 @@ namespace ChamadaApp.API
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-            // Configure Web API to use only bearer token authentication.
-            config.SuppressDefaultHostAuthentication();
-            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            //CORS
+            config.EnableCors(new EnableCorsAttribute("http://localhost:8070", "*", "*"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
@@ -25,6 +21,10 @@ namespace ChamadaApp.API
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Necessário Para Habilitar o Retorno de JSON neste formato: matheusSouzaLima: 
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
         }
     }
 }
--- ChamadaApp.Web/ChamadaApp.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ChamadaApp.API.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
