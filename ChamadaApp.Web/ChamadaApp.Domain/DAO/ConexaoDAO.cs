868a2af2061d574a8e1956bb49a9727def3abc39
Daniel Noronha Canuto <danieldnc_@hotmail.com>
2016-04-25 21:25:47 -0300

    Classe de conexao com o BD criada

---
+++ ChamadaApp.Web/ChamadaApp.Domain/DAO/ConexaoDAO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChamadaApp.Domain.DAO
+{
+    class ConexaoDAO
+    {
+        /// <summary>
+        /// Retorna a conexao com o banco de dados BDTCC
+        /// </summary>
+        /// <returns></returns>
+        public static SqlConnection GetConexao()
+        {
+            string con = "Data Source=localhost;Initial Catalog=BDTCC;User ID=sa;Password=123456";
+            SqlConnection conexao = new SqlConnection(con);
+            conexao.Open();
+
+            return conexao;
+        }
+    }
+}
