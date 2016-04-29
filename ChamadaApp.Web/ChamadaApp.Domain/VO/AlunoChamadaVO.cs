9016921b566c8804620a79286318e21ef837a689
Daniel Noronha Canuto <danieldnc_@hotmail.com>
2016-04-28 22:36:30 -0300

    Substituição das classes VO Tp pela classe TpGenericoVO

---
+++ ChamadaApp.Web/ChamadaApp.Domain/VO/AlunoChamadaVO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChamadaApp.Domain.VO
+{
+    public class AlunoChamadaVO
+    {
+        public AlunoChamadaVO()
+        {
+
+        }
+
+        public int Id { get; set; }
+
+        public int ChamadaId { get; set; }
+
+        public int SitAlunoChamadaId { get; set; }
+
+        public DateTime DtPresenca { get; set; }
+    }
+}
---
+++ ChamadaApp.Web/ChamadaApp.Domain/VO/TpGenericoVO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChamadaApp.Domain.VO
+{
+    /// <summary>
+    /// Essa classe é uma representação das tabelas CURSO, MATERIA, SITALUNO, 
+    /// SITALUNOCHAMADA, SITCHAMADA, SITTURMA e TPUSUARIO presentes no banco de dados...
+    /// </summary>
+    public class TpGenericoVO
+    {
+        public TpGenericoVO()
+        {
+
+        }
+
+        public TpGenericoVO(DataRow registro)
+        {
+            this.Id = (int)registro["ID"];
+            this.Descricao = registro["DESCRICAO"].ToString();
+            this.Ordem = (int)registro["ORDEM"];
+            this.Ativo = (bool)registro["ATIVO"];
+        }
+
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public int Ordem { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
