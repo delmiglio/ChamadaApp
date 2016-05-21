bbccd966cf8220a16de8d33e9a70dabc38565789
Guilherme Oliveira Delmiglio <guidelmiglio@hotmail.com>
2016-05-21 11:09:31 -0300

    Fase 2 Projeto

--- ChamadaApp.Web/ChamadaApp.Domain/VO/UsuarioVO.cs
+++ ChamadaApp.Web/ChamadaApp.Domain/VO/UsuarioVO.cs
@@ -16,8 +16,7 @@ namespace ChamadaApp.Domain.VO
             this.Login = re["LOGIN"].ToString();
             this.Nome = re["NOME"].ToString();
             this.Sobrenome = re["SOBRENOME"].ToString();
-            this.Login = re["LOGIN"].ToString();
-            this.Senha = re["SENHA"].ToString(); 
+            this.Senha = re["SENHA"].ToString();
             this.Token = re["TOKEN"].ToString();
             this.TpUsuario = (int)re["TPUSUARIOID"];
             this.DtCriacao = (DateTime)re["DTCRIACAO"];
@@ -25,7 +24,7 @@ namespace ChamadaApp.Domain.VO
             this.Ativo = (bool)re["ATIVO"];
         }
 
-        
+
         public int Id { get; set; }
 
         public string Nome { get; set; }
@@ -33,17 +32,17 @@ namespace ChamadaApp.Domain.VO
         public string Sobrenome { get; set; }
 
         public string Login { get; set; }
-        
+
         public string Senha { get; set; }
 
         public string Token { get; set; }
-        
+
         public int TpUsuario { get; set; }
-     
+
         public DateTime DtCriacao { get; set; }
-        
+
         public DateTime DtAlteracao { get; set; }
-        
+
         public bool Ativo { get; set; }
 
     }
---
+++ ChamadaApp.Web/ChamadaApp.Web/js/Objetos/Aluno.js
@@ -0,0 +1,42 @@
+﻿var Aluno = function(id, login, nome, sobrenome, senha, token, tpUsuario, dtCriacao, dtAlteracao) {
+   
+    if (id)
+        this.Id = id;
+
+    if (login)
+        this.Login = login;
+
+    if (nome)
+        this.Nome = nome;
+
+    if (sobrenome)
+        this.Sobrenome = sobrenome;
+
+    if (senha)
+        this.Senha = senha;
+
+    if (token)
+        this.Token = token;
+
+    if (tpUsuario)
+        this.tpUsuario = tpUsuario;
+
+    if (dtAlteracao)
+        this.DtAlteracao = dtAlteracao;
+
+    if (dtCriacao)
+        this.DtCriacao = dtCriacao;
+
+    if (ativo)
+        this.Ativo = ativo;
+    
+}
+
+
+function GeraSenhaAluno() {
+
+    var aluno = new Aluno();
+    if (!aluno)
+        PostGenerico("http://localhost:8080/api/aluno/", aluno);
+    
+}
