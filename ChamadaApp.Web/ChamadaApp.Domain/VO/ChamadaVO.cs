bac24bf0f089d98dd791a8808a10fd934b1098e2
Daniel <Daniel@DANIEL>
2016-05-24 13:49:45 -0300

    Mudanças na classe ChamadaVO

--- ChamadaApp.Web/ChamadaApp.Domain/VO/ChamadaVO.cs
+++ ChamadaApp.Web/ChamadaApp.Domain/VO/ChamadaVO.cs
@@ -17,23 +17,35 @@ namespace ChamadaApp.Domain.VO
         public ChamadaVO(DataRow registro)
         {
             this.Id = (int)registro["ID"];
-            this.DtChamada = (DateTime)registro["DTCHAMADA"];
+            this.DtChamada = DataFormatada(registro["DTCHAMADA"].ToString());
             this.HoraInicio = registro["HORAINICIO"].ToString();
-            this.HoraTermino = registro["HORATERMINO"].ToString();
-            this.HorarioMateriaProfTurmaId = (int)registro["HORARIOMATERIAPROFTURMAID"];
-            this.SitChamadaId = (int)registro["SITCHAMADA"];
+            this.HoraTermino = registro["HORATERMINO"].ToString();            
+            this.SitChamada = registro["DESCRICAO"].ToString();
+            this.Materia = registro["MATERIA"].ToString();
+            this.Modulo = registro["MODULO"].ToString();
+            this.Curso = registro["CURSO"].ToString();
         }
 
-        public int Id { get; set; }
+        public int Id { get; set; }        
 
-        public int HorarioMateriaProfTurmaId { get; set; }
-
-        public DateTime DtChamada { get; set; }
+        public string DtChamada { get; set; }
 
         public string HoraInicio { get; set; }
 
         public string HoraTermino { get; set; }
 
-        public int SitChamadaId { get; set; }
+        public string SitChamada { get; set; }
+
+        public string Materia { get; set; }
+
+        public string Modulo { get; set; }
+
+        public string Curso { get; set; }
+
+        private string DataFormatada(string fullDate)
+        {
+            DateTime data = Convert.ToDateTime(fullDate);
+            return data.ToShortDateString();
+        }
     }
 }
