c7a66738c3836a3462a1c7df93a26a07e74398ac
Guilherme Oliveira Delmiglio <guidelmiglio@hotmail.com>
2016-05-26 16:29:46 -0300

    Página Esquematizada e Animação Concluida

---
+++ ChamadaApp.Web/ChamadaApp.Web/js/Metodos/ControleTela.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+
+    $("a").click(function (event) {
+        var link = $(this);
+
+        if (link.attr("id").match("chamada")) {
+            $("#tabela-alunos").hide("slow");
+            $("#tabela-chamada").show("slow");
+        }
+        else if (link.attr("id").match("aluno")) {
+            $("#tabela-chamada").hide("slow");
+            $("#tabela-alunos").show("slow");
+        }
+        else {
+            $("#tabela-alunos").hide("slow");
+            $("#tabela-chamada").hide("slow");
+        }
+
+        event.preventDefault();
+    });
+});
--- ChamadaApp.Web/ChamadaApp.Web/js/Metodos/MontaTabela.js
+++ ChamadaApp.Web/ChamadaApp.Web/js/Metodos/MontaTabela.js
@@ -1,8 +1,7 @@
 ﻿function MontaTabelaGenerica(objeto) {
 
-    //Metodo 4
     var tabela = "<tr> <td> " + objeto.Nome + "</td> <td> " + objeto.Sobrenome + "</td> <td><button class=\"btn btn-primary\" onclick=\"GerarToken()\">Gerar Senha</button></td></tr>";
-    $(tabela).appendTo("#corpoTabela");
+    $(tabela).appendTo("#corpoTabelaAluno");
 
    
 }
