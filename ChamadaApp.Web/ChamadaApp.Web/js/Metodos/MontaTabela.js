7147f513efa507ba938d79ea9a68f04bc3ec9b49
Guilherme Oliveira Delmiglio <guidelmiglio@hotmail.com>
2016-05-26 16:49:08 -0300

    Detalhes

--- ChamadaApp.Web/ChamadaApp.Web/js/Metodos/MontaTabela.js
+++ ChamadaApp.Web/ChamadaApp.Web/js/Metodos/MontaTabela.js
@@ -1,7 +1,16 @@
-﻿function MontaTabelaGenerica(objeto) {
+﻿function MontaTabelaAlunos(objeto) {
 
     var tabela = "<tr> <td> " + objeto.Nome + "</td> <td> " + objeto.Sobrenome + "</td> <td><button class=\"btn btn-primary\" onclick=\"GerarToken()\">Gerar Senha</button></td></tr>";
     $(tabela).appendTo("#corpoTabelaAluno");
 
    
+}
+
+
+function MontaTabelaChamada(objeto) {
+
+    var tabela = "<tr> <td> " + objeto.Nome + "</td> <td> " + objeto.Sobrenome + "</td> <td><button class=\"btn btn-primary\" onclick=\"GerarToken()\">Gerar Senha</button></td></tr>";
+    $(tabela).appendTo("#corpoTabelaChamada");
+
+
 }
