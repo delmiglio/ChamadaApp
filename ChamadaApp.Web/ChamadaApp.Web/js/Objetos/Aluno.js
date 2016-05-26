b0de86765cd0d98baea27b0650231a5c239f967d
Guilherme Oliveira Delmiglio <guidelmiglio@hotmail.com>
2016-05-26 17:10:31 -0300

    correcao

--- ChamadaApp.Web/ChamadaApp.Web/js/Objetos/Aluno.js
+++ ChamadaApp.Web/ChamadaApp.Web/js/Objetos/Aluno.js
@@ -1,40 +1,4 @@
-﻿var Aluno = function (id, login, nome, sobrenome, senha,
-    token, tpUsuario, dtCriacao, dtAlteracao, ativo) {
-
-    if (id)
-        this.Id = 1;
-
-    if (login)
-        this.Login = "login";
-
-    if (nome)
-        this.Nome = "nome";
-
-    if (sobrenome)
-        this.Sobrenome = sobrenome;
-
-    if (senha)
-        this.Senha = senha;
-
-    if (token)
-        this.Token = token;
-
-    if (tpUsuario)
-        this.tpUsuario = tpUsuario;
-
-    if (dtAlteracao)
-        this.DtAlteracao = dtAlteracao;
-
-    if (dtCriacao)
-        this.DtCriacao = dtCriacao;
-
-    if (ativo)
-        this.Ativo = 1;
-
-}
-
-
-function GerarToken() {
+﻿function GerarToken() {
 
     //var aluno = new Aluno();
     var resultado;
@@ -48,7 +12,7 @@ function GerarToken() {
 
 function GetAlunos(ra, nome) {
 
-    $("#corpoTabela").empty();
+    $("#corpoTabelaAluno").empty();
     var ra = $("#RA").val();
     var nome = $("#Nome").val();
     var url = "http://localhost:8090/api/usuario";
@@ -63,7 +27,7 @@ function GetAlunos(ra, nome) {
 
     if (alunos.ListRetorno.length > 0) {
         for (var i = 0; i < alunos.ListRetorno.length; i++) {
-            MontaTabelaGenerica(alunos.ListRetorno[i]);
+            MontaTabelaAlunos(alunos.ListRetorno[i]);
         }
     }
     else {
